e62cde4c9d5f6caa183ca71f23cb2e8650eba7aa
Arisson Santos <111756983+ArissonSantos@users.noreply.github.com>
2026-04-29 11:46:54 -0300

    29-04
    Introdução

---
+++ 29-04.py
@@ -0,0 +1,16 @@
+# nome = input("Digite seu nome: ")
+#print("Olá mundo, sou: ", nome)
+
+#idade = int(input("Idade: "))
+#print("Sua idade: ", idade)
+
+#if idade >= 18:
+#    for i in range(10): 
+#        print("Você é adulto.")
+#else:
+#    print("Get out child!") 
+#numero=0
+#while numero <50:
+#    print(numero)
+#    numero=numero+1
+
